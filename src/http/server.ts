fcaad3fb7a477ef0f2ca2d54fa2d51ed61770904
DanielMctavish <daniel_arruda@outlook.pt>
2023-10-06 17:49:29 -0300

    cors-access

--- src/http/server.ts
+++ src/http/server.ts
@@ -11,6 +11,15 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://v-codes-chatbot-feature.vercel.app/');
+    //res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST,PATCH, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 app.get("/", (req, res) => { res.status(200).send("Arboris_codex/VCodes API whatsapp v1.0") })
 app.use("/whatsapp", whatsapp_routes)
 app.use("/stream", stream_routes)
